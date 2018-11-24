fd26ea6a43ebfd5776e3870afa8bc3242b15a3a9
Riley PC <riley-ball@outlook.com>
2018-11-24 15:05:33 +1000

    updated to connect to correct stream

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const channel = "tfue";
+var channel;
+var docURL = document.URL;
+var len = docURL.length;
+
+var test;
+var i;
+for (i = len; i > 0; i--) {
+    if (docURL[i] == "/") {
+            channel = "#" + docURL.substring(i + 1, len)
+            break
+    }
+}
 
 // Require the TwitchJS library.
 const TwitchJS = require('twitch-js');
@@ -22,7 +33,7 @@ const options = {
   //   username: 'twitchapis',
   //   password: TOKEN,
   // },
-  channels: [`#${channel}`]
+  channels: [channel]
 };
 
 const client = new TwitchJS.client(options);
